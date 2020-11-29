4dde4996c8c50d83aa7cbb22ed5c48a9ffaca64c
Alexander Almström <alexander.almstrom@panagora.se>
2020-11-29 18:29:09 +0100

    Move app css import

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "./App.css";
 import { GlobalContext } from "./context/GlobalContext";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
-
-import "./App.css";
 
 const App = () => {
   const products = [
